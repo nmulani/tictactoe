df5d61f091bc584ccc1ecc5d90541ae6455f23e1
nmulani <nikhilrmulani@gmail.com>
2017-01-16 22:25:18 -0500

    Added two-player mode

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Using what I've learned in Haskell so far, I've coded a simple game of TicTacToe, where the user plays against the computer. To play, go to the source directory in your terminal, and then type "ghc tictactoe" or "stack ghc tictactoe" to compile.
 
-Use "./tictactoe E" to run the game in "Easy" mode, where the computer will not actively block your moves, but will only try to arrange its own pieces in a winning combination. Use "./tictactoe H" to run the game in Hard mode, where the computer will try to block you from winning, and will also try to win with its own pieces. In both cases, the computer moves second, so that the user has a chance of winning:
+Use "./tictactoe E" to run the game in "Easy" mode, where the computer will not actively block your moves, but will only try to arrange its own pieces in a winning combination. Use "./tictactoe H" to run the game in Hard mode, where the computer will try to block you from winning, and will also try to win with its own pieces. In both cases, the computer moves second, so that the user has a chance of winning. Use "./tictactoe P" to play in two-player mode.
 
 ```
 
@@ -22,4 +22,3 @@ Some possible opportunities for improvement:
 - Color code tic-tac-toe board
 - Give the user a hint about best next move
 - Add a GUI interface
-- Allow two people to play each other
--- tictactoe.hs
+++ tictactoe.hs
@@ -22,26 +22,30 @@ import System.Exit
 type Matrix = [[Int]]
 type Position = (Int,Int)
 
--- Our Xs and Os are represented by 1s and 2s. User is 1, Computer is 2
+
+-- Our Xs and Os are represented by 1s and 2s. User is 1, Second Player is 2
 -- The starting board will only have zeroes.
 userNum = 1  --X
-compNum = 2  --O
+secondPlayer = 2  --O
 startingBoard = [[0,0,0],[0,0,0],[0,0,0]]
 
--- To run the program, we can just run "tictactoe E" or "tictactoe H".
+-- To run the program, we can just run "tictactoe E", "tictactoe H" or "tictactoe P".
 -- Any other argument will run in Easy mode by default
 main :: IO ()
 main = do args <- getArgs
           case args of
              ["H"] -> do clearScreen
                          putStrLn "Playing in hard mode"
-                         playGame startingBoard "H"
+                         playGame startingBoard "H" userNum
              ["E"] -> do clearScreen
                          putStrLn "Playing in easy mode"
-                         playGame startingBoard "E"
+                         playGame startingBoard "E" userNum
+             ["P"] -> do clearScreen
+                         putStrLn "Playing in two-player mode"
+                         playGame startingBoard "P" userNum
              _ -> do clearScreen
                      putStrLn "Arguments not understood. Playing in default easy mode."
-                     playGame startingBoard "E"
+                     playGame startingBoard "E" userNum
 
 -- Main program flow (IO):
 --
@@ -53,35 +57,40 @@ main = do args <- getArgs
 -- Computer makes a move
 -- This is a recursive function
 
-playGame :: Matrix -> String -> IO ()
-playGame board difficultyLevel = do putStrLn "Here's what the board looks like:\n"
-                                    showBoardState board
-                                    -- check for Wins and Ties after each move
-                                    checkWinsTies board
-                                    (x, y) <- getUserMove
-                                    case checkUserMove board (x, y) of
+playGame :: Matrix -> String -> Int -> IO ()
+playGame board playMode whichUser = do putStrLn "Here's what the board looks like:\n"
+                                       showBoardState board
+                                       -- check for Wins and Ties after each move
+                                       checkWinsTies board
+                                       (x, y) <- getUserMove
+                                       case checkUserMove board (x, y) whichUser of
                                                   [[3]] -> do clearScreen
                                                               putStrLn "There's already a piece there! Try another spot on the board."
-                                                              playGame board difficultyLevel
+                                                              playGame board playMode whichUser
                                                   [[4]] -> do clearScreen
                                                               putStrLn "Hmm, I can't find that spot on the board. Maybe you made a typo? Try again."
-                                                              playGame board difficultyLevel
+                                                              playGame board playMode whichUser
                                                   [[5]] -> do clearScreen
                                                               putStrLn "Something strange happened. Let's try again."
-                                                              playGame board difficultyLevel
+                                                              playGame board playMode whichUser
                                                 -- If user inputs something other than H or E when running main, we'll by default
                                                 -- play in easy mode.
                                                 -- Check for wins and ties after each move
-                                                  a -> case difficultyLevel of
+                                                  a -> case playMode of
                                                             "H" -> do clearScreen
                                                                       checkWinsTies a
-                                                                      (playGame (getHardComputerMove (a)) difficultyLevel)
+                                                                      (playGame (getHardComputerMove (a)) playMode userNum)
                                                             "E" -> do clearScreen
                                                                       checkWinsTies a
-                                                                      (playGame (getEasyComputerMove (a)) difficultyLevel)
+                                                                      (playGame (getEasyComputerMove (a)) playMode userNum)
+                                                            "P" -> do clearScreen
+                                                                      checkWinsTies a
+                                                                      if (whichUser == 1)
+                                                                        then do (playGame a playMode 2)
+                                                                      else do (playGame a playMode 1)
                                                             otherwise -> do clearScreen
                                                                             checkWinsTies a
-                                                                            (playGame (getEasyComputerMove (a)) "E")
+                                                                            (playGame (getEasyComputerMove (a)) "E" userNum)
 
 -- Once the game is over, check if the user wants to play again
 playAgain :: IO()
@@ -94,15 +103,17 @@ playAgain = do putStrLn "Would you like to play again? Y/N"
                   otherwise -> do putStrLn "I didn't really understand that..come again?"
                                   playAgain
 
--- Ask if user would like to play in Easy or Hard mode
+-- Ask if user would like to play in Easy, Hard mode, or 2-player mode
 whichMode :: IO()
-whichMode = do putStrLn "Would you like to play in easy or hard mode? E/H"
+whichMode = do putStrLn "Would you like to play in easy mode, hard mode, or player vs. player? E/H/P"
                answer <- getLine
                case answer of
                    "E" -> do clearScreen
-                             playGame startingBoard "E"
+                             playGame startingBoard "E" userNum
                    "H" -> do clearScreen
-                             playGame startingBoard "H"
+                             playGame startingBoard "H" userNum
+                   "P" -> do clearScreen
+                             playGame startingBoard "P" userNum
                    otherwise -> do putStrLn "I didn't really understand that..come again?"
                                    whichMode
 
@@ -125,7 +136,7 @@ getUserMove = do putStrLn "Your move! A few pointers: "
 
 -- Check for wins and ties
 checkWinsTies :: Matrix -> IO ()
-checkWinsTies board = if (wasThereAWin board compNum)
+checkWinsTies board = if (wasThereAWin board secondPlayer)
                          then do putStrLn "The computer won this time...better luck next time!"
                                  playAgain
                       else if (wasThereAWin board userNum)
@@ -151,7 +162,7 @@ checkPiece :: Matrix -> Position -> Int
 checkPiece board (y, x) = board !! y !! x
 
 -- Perform a move for the user or computer on specified location on the board.
--- We'll increment the zero a certain amount (userNum or compNum)
+-- We'll increment the zero a certain amount (userNum or secondPlayer)
 performMove :: Position -> (a->a) -> [[a]] -> [[a]]
 performMove (y, x) f = boardSurgery y (\ycoord -> boardSurgery x f ycoord)
 
@@ -171,29 +182,29 @@ changeHead f (a:as) = f a : as
 doMove :: (Int, Int) -> Int -> Matrix -> Matrix
 doMove (a, b) computerOrUser board
   | ((checkPiece board (a, b)) == 0) = ((performMove (a, b) (\a->a+computerOrUser) board))
-  | (checkPiece board (a, b) == compNum) = [[0]] -- Signal there's already a piece on the board
+  | (checkPiece board (a, b) == secondPlayer) = [[0]] -- Signal there's already a piece on the board
   | (checkPiece board (a, b) == userNum) = [[0]] -- Signal there's already a piece on the board
   | otherwise = [[1]] -- Signal there was an input error of some kind
 
 -- Helper function for doMove for the user
 -- Figure out what to do with user move coordinates
-checkUserMove :: Matrix -> Position -> Matrix
-checkUserMove board tuple = do
-                       if (((doMove (tuple) userNum board) /= [[0]]) && ((doMove (tuple) userNum board) /= [[1]]))
-                                  then do (doMove (tuple) userNum board)
-                       else if ((doMove (tuple) userNum board) == [[0]])
+checkUserMove :: Matrix -> Position -> Int -> Matrix
+checkUserMove board tuple whichUser = do
+                       if (((doMove (tuple) whichUser board) /= [[0]]) && ((doMove (tuple) whichUser board) /= [[1]]))
+                                  then do (doMove (tuple) whichUser board)
+                       else if ((doMove (tuple) whichUser board) == [[0]])
                           then do [[3]]
-                       else if ((doMove (tuple) userNum board) == [[1]])
+                       else if ((doMove (tuple) whichUser board) == [[1]])
                           then do [[4]]
                        else do [[5]]
 
 -- Helper method for doMove, calculating computer's next move (hard)
 getHardComputerMove :: Matrix -> Matrix
-getHardComputerMove board = doMove (computerHardNextMove board) compNum board
+getHardComputerMove board = doMove (computerHardNextMove board) secondPlayer board
 
 -- Helper method for doMove, calculating computer's next move (easy)
 getEasyComputerMove :: Matrix -> Matrix
-getEasyComputerMove board = doMove (computerEasyNextMove board) compNum board
+getEasyComputerMove board = doMove (computerEasyNextMove board) secondPlayer board
 
 
 
@@ -206,7 +217,7 @@ getEasyComputerMove board = doMove (computerEasyNextMove board) compNum board
 printRow :: [Int] -> String
 printRow [] = "\n"
 printRow [a]
-    | (a == compNum) = show "O"
+    | (a == secondPlayer) = show "O"
     | (a == userNum) = show "X"
     | otherwise = show "E"
 printRow (a:as) = (printRow [a]) ++ " " ++ (printRow as) ++ "\n"
@@ -228,7 +239,7 @@ showBoardState board = do putStr (printMatrix board)
 ----- wasThereATie
 
 
--- Detect Win. Where Int should equal userNum for the user, and compNum for the computer
+-- Detect Win. Where Int should equal userNum for the user, and secondPlayer for the computer
 wasThereAWin :: Matrix -> Int -> Bool
 wasThereAWin [a,b,c] d
   | all (\y->(y==d)) a = True
@@ -309,9 +320,9 @@ computerEasyNextMove [a,b,c]
 -- Does the computer have an opportunity for a horizontal win?
 rowWinner :: Matrix -> Position
 rowWinner [a,b,c]
-  | ((rowNextWinningMove a compNum 0) /= (4,4)) = (rowNextWinningMove a compNum 0)
-  | ((rowNextWinningMove b compNum 1) /= (4,4)) = (rowNextWinningMove b compNum 1)
-  | ((rowNextWinningMove c compNum 2) /= (4,4)) = (rowNextWinningMove c compNum 2)
+  | ((rowNextWinningMove a secondPlayer 0) /= (4,4)) = (rowNextWinningMove a secondPlayer 0)
+  | ((rowNextWinningMove b secondPlayer 1) /= (4,4)) = (rowNextWinningMove b secondPlayer 1)
+  | ((rowNextWinningMove c secondPlayer 2) /= (4,4)) = (rowNextWinningMove c secondPlayer 2)
   | otherwise = (4,4)
 
 rowNextWinningMove :: [Int] -> Int -> Int -> Position
@@ -327,9 +338,9 @@ rowNextWinningMove a d rowIndex
 -- Does the computer have an opportunity for a vertical win?
 verticalWinner :: Matrix -> Position
 verticalWinner [a,b,c]
- | ((verticalNextWinningMove [a,b,c] 0 compNum) /= (4,4)) = (verticalNextWinningMove [a,b,c] 0 compNum)
- | ((verticalNextWinningMove [a,b,c] 1 compNum) /= (4,4)) = (verticalNextWinningMove [a,b,c] 1 compNum)
- | ((verticalNextWinningMove [a,b,c] 2 compNum) /= (4,4)) = (verticalNextWinningMove [a,b,c] 2 compNum)
+ | ((verticalNextWinningMove [a,b,c] 0 secondPlayer) /= (4,4)) = (verticalNextWinningMove [a,b,c] 0 secondPlayer)
+ | ((verticalNextWinningMove [a,b,c] 1 secondPlayer) /= (4,4)) = (verticalNextWinningMove [a,b,c] 1 secondPlayer)
+ | ((verticalNextWinningMove [a,b,c] 2 secondPlayer) /= (4,4)) = (verticalNextWinningMove [a,b,c] 2 secondPlayer)
  | otherwise = (4,4)
 
 verticalNextWinningMove :: Matrix -> Int -> Int -> Position
@@ -345,7 +356,7 @@ verticalNextWinningMove [a,b,c] column d
  -- Does the computer have an opportunity for a diagonal win?
 diagonalWinner :: Matrix -> Position
 diagonalWinner [a,b,c]
-  | ((diagonalNextWinningMove [a,b,c] compNum) /= (4,4)) = (diagonalNextWinningMove [a,b,c] compNum)
+  | ((diagonalNextWinningMove [a,b,c] secondPlayer) /= (4,4)) = (diagonalNextWinningMove [a,b,c] secondPlayer)
   | otherwise = (4,4)
 
 diagonalNextWinningMove :: Matrix -> Int -> Position
@@ -364,9 +375,9 @@ diagonalNextWinningMove [a,b,c] d
 -- Find a strategic empty slot next to one of the computer's pieces in a row
 rowStrategicNext :: Matrix -> Position
 rowStrategicNext [a,b,c]
-  | (findGoodEmptySpaceRow a compNum 0 /= (4,4)) = (findGoodEmptySpaceRow a compNum 0)
-  | (findGoodEmptySpaceRow b compNum 1 /= (4,4)) = (findGoodEmptySpaceRow b compNum 1)
-  | (findGoodEmptySpaceRow c compNum 2 /= (4,4)) = (findGoodEmptySpaceRow c compNum 2)
+  | (findGoodEmptySpaceRow a secondPlayer 0 /= (4,4)) = (findGoodEmptySpaceRow a secondPlayer 0)
+  | (findGoodEmptySpaceRow b secondPlayer 1 /= (4,4)) = (findGoodEmptySpaceRow b secondPlayer 1)
+  | (findGoodEmptySpaceRow c secondPlayer 2 /= (4,4)) = (findGoodEmptySpaceRow c secondPlayer 2)
   | otherwise = (4,4)
 
 findGoodEmptySpaceRow :: [Int] -> Int -> Int -> Position
@@ -383,7 +394,7 @@ findGoodEmptySpaceRow a d rowIndex
 -- Find a strategic empty slot diagonal to one of the computer's pieces
 diagonalStrategicNext :: Matrix -> Position
 diagonalStrategicNext [a,b,c]
-  | (findGoodEmptySpaceDiagonal [a,b,c] compNum /= (4,4)) = (findGoodEmptySpaceDiagonal [a,b,c] compNum)
+  | (findGoodEmptySpaceDiagonal [a,b,c] secondPlayer /= (4,4)) = (findGoodEmptySpaceDiagonal [a,b,c] secondPlayer)
   | otherwise = (4,4)
 
 findGoodEmptySpaceDiagonal :: Matrix -> Int -> Position
@@ -402,9 +413,9 @@ findGoodEmptySpaceDiagonal [a,b,c] d
 -- Find a strategic empty vertical slot next to one of the computer's pieces
 verticalStrategicNext :: Matrix -> Position
 verticalStrategicNext [a,b,c]
-  | (verticalNextGoodEmptySpace [a,b,c] 0 compNum /= (4,4)) = (verticalNextGoodEmptySpace [a,b,c] 0 compNum)
-  | (verticalNextGoodEmptySpace [a,b,c] 1 compNum /= (4,4)) = (verticalNextGoodEmptySpace [a,b,c] 1 compNum)
-  | (verticalNextGoodEmptySpace [a,b,c] 2 compNum /= (4,4)) = (verticalNextGoodEmptySpace [a,b,c] 2 compNum)
+  | (verticalNextGoodEmptySpace [a,b,c] 0 secondPlayer /= (4,4)) = (verticalNextGoodEmptySpace [a,b,c] 0 secondPlayer)
+  | (verticalNextGoodEmptySpace [a,b,c] 1 secondPlayer /= (4,4)) = (verticalNextGoodEmptySpace [a,b,c] 1 secondPlayer)
+  | (verticalNextGoodEmptySpace [a,b,c] 2 secondPlayer /= (4,4)) = (verticalNextGoodEmptySpace [a,b,c] 2 secondPlayer)
   | otherwise = (4,4)
 
 verticalNextGoodEmptySpace :: Matrix -> Int -> Int -> Position
